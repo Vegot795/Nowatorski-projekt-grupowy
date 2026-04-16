7be798b71cf447d9b325b5f735ea5184812ba0cf
Davies <167176338+Vegot795@users.noreply.github.com>
2026-04-16 10:09:55 +0200

    Reapply "Merge remote-tracking branch 'origin/Inventory' into FarmMechanicSimulator"
    This reverts commit c5811adc3789049d2750181224caa032a5a9ee0c.

---
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+public class InventorySlot : MonoBehaviour
+{
+    [field: SerializeField] public bool IsOccupied { get; private set; }
+    [field: SerializeField] public ItemSO ItemInSlot { get; private set; }
+    [field: SerializeField] public int ItemAmount { get; private set; }
+
+    private void changeOccupancy(bool occupancyValue)
+    {
+        IsOccupied = occupancyValue;
+    }
+    public void AddItemAmount(int amount)
+    {
+        ItemAmount += amount;
+    }
+    public void RemoveItemAmount(int amount)
+    {
+        ItemInSlot = null;
+        ItemAmount -= amount;
+    }
+    public void AddItem(ItemSO item, int amount)
+    {
+        ItemInSlot = item;
+        ItemAmount += amount;
+    }
+    public void RemoveItem()
+    {
+        ItemInSlot = null;
+        ItemAmount = 0;
+    }
+
+}
