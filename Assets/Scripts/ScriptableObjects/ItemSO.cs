daab4288c03dfe6b6927e19f2e2c8c89d0e0233d
Pavixon <2pawelnizio@gmail.com>
2026-06-09 13:58:40 +0200

    Added ShopSlotSript, CurrencyController and updated ItemSO

--- Assets/Scripts/ScriptableObjects/ItemSO.cs
+++ Assets/Scripts/ScriptableObjects/ItemSO.cs
@@ -6,4 +6,13 @@ public class ItemSO : ScriptableObject
     public Sprite Icon;
     public string Description;
     public ushort MaxStackAmount;
+    public int buyPrice = 10;
+    [Range(0, 1)]
+    public float sellPriceMultiplier = 0.5f;
+
+    public int GetSellPrice()
+    {
+        return Mathf.RoundToInt(buyPrice * sellPriceMultiplier);
+    }
 }
+
