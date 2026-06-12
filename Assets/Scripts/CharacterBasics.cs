904179e3d901a2aa928ace97352703e49b616cce
Vegot795 <167176338+Vegot795@users.noreply.github.com>
2026-06-13 01:59:18 +0200

    Naprawa nasion

--- Assets/Scripts/CharacterBasics.cs
+++ Assets/Scripts/CharacterBasics.cs
@@ -1,4 +1,6 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterBasics : MonoBehaviour
 {
@@ -13,12 +15,45 @@ public class CharacterBasics : MonoBehaviour
     private bool isKnockedBack = false;
     private float knockbackTimer = 0f;
     private float knockbackDuration = 0.2f;
+    private MobSpawner spawner;
+    [SerializeField] private Slider hpSlider;
+    private bool isPlayer;
 
     void Start()
     {
         CurrentHP = MaxHP;
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+        if(spawner == null)
+        {
+            spawner = FindAnyObjectByType<MobSpawner>();
+        }
+
+        if (gameObject.CompareTag("Player"))
+        {
+            Debug.Log("GameObject is player");
+            isPlayer = true;
+
+            if(hpSlider == null)
+            {
+                hpSlider = GameObject.FindWithTag("HealthSlider").GetComponent<Slider>();
+            }
+        }
+        else
+        {
+            hpSlider = GetComponentInChildren<Slider>();
+        }
+
+        if (hpSlider != null)
+        {
+            hpSlider.maxValue = MaxHP;
+            hpSlider.value = CurrentHP;
+        }
+        else
+        {
+            Debug.Log($"SLIDER IS MISSING FOR {gameObject}");
+        }
+        
     }
 
     void Update()
@@ -44,10 +79,26 @@ public class CharacterBasics : MonoBehaviour
         KnockBack(knockbackDirection, KnockBackDistance);
         isKnockedBack = true;
         knockbackTimer = knockbackDuration;
+        hpSlider.value = CurrentHP;
+        if (isPlayer)
+        {
+            Image fillImage = hpSlider.fillRect.GetComponent<Image>();
+            if (fillImage != null)
+            {
+                float hpPerCent = (float)CurrentHP / MaxHP;
+                Color max = new Color(12f / 255f, 255f / 255f, 0f / 255f, 1f);      // Green #0CFF00
+                Color min = new Color(255f / 255f, 0f / 255f, 20f / 255f, 1f);      // Red #FF0014
+                fillImage.color = Color.Lerp(min, max, hpPerCent);
+            }
+        }
     }
 
     private void Die()
     {
+        if (gameObject == Resources.Load<GameObject>("FlyingEye"))
+        {
+            spawner.RemoveEnemyFromList(gameObject.GetComponent<FlyingEyeController>());
+        }
         Destroy(gameObject);
     }
 
@@ -77,5 +128,10 @@ public class CharacterBasics : MonoBehaviour
         position.y = data.DataPlayerPosition[1];
         position.z = data.DataPlayerPosition[2];
         transform.position = position;
+
+        if (hpSlider != null)
+        {
+            hpSlider.value = CurrentHP;
+        }
     }
 }
